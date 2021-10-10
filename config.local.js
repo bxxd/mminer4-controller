a4e420a2021a2e2b72dc0581d471c0077e74d37f
bxxd <bxxd.eth@gmail.com>
2021-10-10 08:46:54 -0400

    update

---
+++ config.local.js
@@ -0,0 +1,8 @@
+var config = {};
+
+// config.addresses = [];
+// config.address.push({
+//   address: "0xE8946EC499a839c72E60bA7d437E28cd73a3f487",
+//   type: "email",
+//   email: "bri4n.r33d@gmail.com",
+// });
