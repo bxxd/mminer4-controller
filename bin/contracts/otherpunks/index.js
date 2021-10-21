2d1c2725e2e829ecc675909931f393a77d1f0178
bxxd <bxxd.eth@gmail.com>
2021-10-20 20:40:24 -0400

    delete

--- bin/contracts/otherpunks/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Otherpunks__factory = void 0;
-var Otherpunks__factory_1 = require("./factories/Otherpunks__factory");
-Object.defineProperty(exports, "Otherpunks__factory", { enumerable: true, get: function () { return Otherpunks__factory_1.Otherpunks__factory; } });
-//# sourceMappingURL=index.js.map
